6d2cff580513c1e2331b73834e8435e635908644
Marie Serykh <marie.serykh@gmail.com>
2021-08-21 19:47:30 +0200

    feat: add partial solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
+const operations = {
+    '+' : (a, b) => a + b,
+    '-' : (a, b) => a - b,
+    '*' : (a, b) => a * b,
+    '/' : (a, b) => a / b,
+};
 
 function expressionCalculator(expr) {
-    // write your solution here
+    const expression = expr.replace(/\s/g, '');
+
+    if (expression.includes('/0')) {
+        throw "TypeError: Division by zero.";
+    }
+
+    const symbols = expression.split('').filter(element => element === '+' || element === '-' || element === '*' || element === '/');
+    
+    const operands = expression.split(/[+,\-,*,\/]/);
+    console.log(operands);
+    console.log(symbols);
+    let result = Number(operands[0]);
+    for (let i = 0; i < symbols.length; i++) {
+        result = operations[symbols[i]](result, Number(operands[i + 1]));
+    }
+
+    return result;
 }
 
 module.exports = {
