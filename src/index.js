dc87accff7b16c7ff2d3640dc9b268cfe016bd71
Marie Serykh <marie.serykh@gmail.com>
2021-08-22 11:13:07 +0200

    feat: add checks if there is a division by 0 and if brackets are paired

--- src/index.js
+++ src/index.js
@@ -5,18 +5,30 @@ const operations = {
     '/' : (a, b) => a / b,
 };
 
-function expressionCalculator(expr) {
-    const expression = expr.replace(/\s/g, '');
-
+function checkDivisionByZero(expression) {
     if (expression.includes('/0')) {
         throw "TypeError: Division by zero.";
     }
+}
+
+function checkBrackets(expression) {
+    const bracketsLeft = expression.match(/[(]/g);
+    const bracketsRight = expression.match(/[)]/g);
+
+    if (bracketsLeft === null || bracketsRight === null || bracketsLeft.length !== bracketsRight.length) {
+        throw "ExpressionError: Brackets must be paired";
+    }
+}
+
+function expressionCalculator(expr) {
+    const expression = expr.replace(/\s/g, '');
+
+    checkDivisionByZero(expression);
+    if (/[(,)]/.test(expression)) checkBrackets(expression);
 
     const symbols = expression.split('').filter(element => element === '+' || element === '-' || element === '*' || element === '/');
-    
     const operands = expression.split(/[+,\-,*,\/]/);
-    console.log(operands);
-    console.log(symbols);
+    
     let result = Number(operands[0]);
     for (let i = 0; i < symbols.length; i++) {
         result = operations[symbols[i]](result, Number(operands[i + 1]));
